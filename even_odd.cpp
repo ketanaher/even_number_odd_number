b0840e568e01d98b365d631bc26ba229a6b4e1a0
ketanaher <141157327+ketanaher@users.noreply.github.com>
2023-08-01 21:14:29 +0530

    Add files via upload

---
+++ even_odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the number"<<endl;
+    cin>>num;
+    if(num%2==0){
+        cout<<"The number is even number"<<endl;
+    }
+    else{
+        cout<<"The number is odd number"<<endl;
+    }
+    return 0;
+}
